c05861ceef736bc88237ea3ccc4a6ea90d2322c8
Rizki Cahya <rizki.cahya03@gmail.com>
2025-12-15 14:24:41 +0700

    add: mahasiswa controller

---
+++ Controller/MahasiswaController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using PerpustakaanApp.Model.Entity;
+using PerpustakaanApp.Model.Repository;
+using PerpustakaanApp.Model.Context;
+
+namespace PerpustakaanApp.Controller
+{
+    public class MahasiswaController
+    {
+        // deklarasi objek repository untuk menjalankan operasi CRUD
+        private MahasiswaRepository _repository;
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nim))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Jurusan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Create(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+    }
+}

7d0aefa15838c3aea283d86dc22197ae1e27e4cc
Rizki Cahya <rizki.cahya03@gmail.com>
2025-12-15 14:35:27 +0700

    feat: add update, delete, readAll, readByName feature in controller

--- Controller/MahasiswaController.cs
+++ Controller/MahasiswaController.cs
@@ -64,5 +64,98 @@ namespace PerpustakaanApp.Controller
             return result;
         }
 
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            if (string.IsNullOrEmpty(mhs.Nim))
+            {
+                MessageBox.Show("NIM harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(mhs.Jurusan))
+            {
+                MessageBox.Show("Jurusan harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Update(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil diupdate !", "Informasi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal diupdate !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            return result;
+        }
+        
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            if (string.IsNullOrEmpty(mhs.Nim))
+            {
+                MessageBox.Show("NIM harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Delete(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil dihapus !", "Informasi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal dihapus !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            return result;
+        }
+
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                list = _repository.ReadByNama(nama);
+            }
+            return list;
+        }
+
+        public List<Mahasiswa> ReadAll()
+        {
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                list = _repository.ReadAll();
+            }
+            return list;
+        }
     }
 }
